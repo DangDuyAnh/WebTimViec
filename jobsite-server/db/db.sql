80c9c62f6db357fbd0e08948f547eb4d297745b3
Nguyen Huu Kiet <kietnguyen10112000@gmail.com>
2022-06-28 15:39:43 +0700

    refresh db.sql

--- jobsite-server/db/db.sql
+++ jobsite-server/db/db.sql
@@ -156,7 +156,7 @@ CREATE TABLE IF NOT EXISTS `auth_user` (
   UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
 
--- Dumping data for table jobsite-db.auth_user: ~1 rows (approximately)
+-- Dumping data for table jobsite-db.auth_user: ~0 rows (approximately)
 /*!40000 ALTER TABLE `auth_user` DISABLE KEYS */;
 INSERT INTO `auth_user` (`id`, `password`, `last_login`, `is_superuser`, `username`, `first_name`, `last_name`, `email`, `is_staff`, `is_active`, `date_joined`) VALUES
 	(1, 'pbkdf2_sha256$320000$SFmZUpsi4sJXRIDMXa0HpM$mjYBEBfzaYtx2z2I02sxOnkjnaZ6sMjLCwsVBkRVAV4=', NULL, 1, 'admin', '', '', '', 1, 1, '2022-06-12 14:09:27.956928');
@@ -193,6 +193,26 @@ CREATE TABLE IF NOT EXISTS `auth_user_user_permissions` (
 -- Dumping data for table jobsite-db.auth_user_user_permissions: ~0 rows (approximately)
 /*!40000 ALTER TABLE `auth_user_user_permissions` DISABLE KEYS */;
 /*!40000 ALTER TABLE `auth_user_user_permissions` ENABLE KEYS */;
+
+-- Dumping structure for table jobsite-db.company
+CREATE TABLE IF NOT EXISTS `company` (
+  `ID` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(256) COLLATE utf16_unicode_ci DEFAULT NULL,
+  `address` varchar(256) COLLATE utf16_unicode_ci DEFAULT NULL,
+  `province_id` bigint(20) unsigned NOT NULL,
+  `desc` varchar(256) COLLATE utf16_unicode_ci NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT 0 COMMENT '0 - wait, 1 - accept, 2 - reject',
+  PRIMARY KEY (`ID`),
+  KEY `FK_company_province` (`province_id`) USING BTREE,
+  CONSTRAINT `FK_company_province` FOREIGN KEY (`province_id`) REFERENCES `province` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
+
+-- Dumping data for table jobsite-db.company: ~2 rows (approximately)
+/*!40000 ALTER TABLE `company` DISABLE KEYS */;
+INSERT INTO `company` (`ID`, `name`, `address`, `province_id`, `desc`, `status`) VALUES
+	(1, 'Công ty 1', 'số 1 ngõ Độc Lập, đường Tự Do, quận Hạnh Phúc', 1, 'công ty test 1', 1),
+	(2, 'Công ty 2', 'Thiên Đàng', 2, 'công ty test 2', 1);
+/*!40000 ALTER TABLE `company` ENABLE KEYS */;
 
 -- Dumping structure for table jobsite-db.django_admin_log
 CREATE TABLE IF NOT EXISTS `django_admin_log` (
@@ -249,7 +269,7 @@ CREATE TABLE IF NOT EXISTS `django_migrations` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
 
--- Dumping data for table jobsite-db.django_migrations: ~23 rows (approximately)
+-- Dumping data for table jobsite-db.django_migrations: ~20 rows (approximately)
 /*!40000 ALTER TABLE `django_migrations` DISABLE KEYS */;
 INSERT INTO `django_migrations` (`id`, `app`, `name`, `applied`) VALUES
 	(1, 'contenttypes', '0001_initial', '2022-06-12 13:59:43.595798'),
@@ -289,6 +309,124 @@ CREATE TABLE IF NOT EXISTS `django_session` (
 -- Dumping data for table jobsite-db.django_session: ~0 rows (approximately)
 /*!40000 ALTER TABLE `django_session` DISABLE KEYS */;
 /*!40000 ALTER TABLE `django_session` ENABLE KEYS */;
+
+-- Dumping structure for table jobsite-db.employee
+CREATE TABLE IF NOT EXISTS `employee` (
+  `ID` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) unsigned NOT NULL,
+  `date_of_birth` datetime DEFAULT NULL,
+  `gender` varchar(16) COLLATE utf16_unicode_ci DEFAULT NULL,
+  `image_link` varchar(256) COLLATE utf16_unicode_ci DEFAULT NULL,
+  `literacy` varchar(64) COLLATE utf16_unicode_ci DEFAULT NULL,
+  `specialist_knowledge` varchar(256) COLLATE utf16_unicode_ci DEFAULT NULL,
+  `experience` varchar(256) COLLATE utf16_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`ID`) USING BTREE,
+  KEY `FK_employee_user` (`user_id`),
+  CONSTRAINT `FK_employee_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
+
+-- Dumping data for table jobsite-db.employee: ~2 rows (approximately)
+/*!40000 ALTER TABLE `employee` DISABLE KEYS */;
+INSERT INTO `employee` (`ID`, `user_id`, `date_of_birth`, `gender`, `image_link`, `literacy`, `specialist_knowledge`, `experience`) VALUES
+	(1, 4, '2022-06-28 10:17:09', 'male', 'image/test1.png', '12/12', 'nothing', '0 years'),
+	(2, 6, '2022-06-28 10:18:50', 'female', 'image/test2.png', '1/12', 'smile', '100 years');
+/*!40000 ALTER TABLE `employee` ENABLE KEYS */;
+
+-- Dumping structure for table jobsite-db.employee_applied_job
+CREATE TABLE IF NOT EXISTS `employee_applied_job` (
+  `employee_id` bigint(20) unsigned NOT NULL,
+  `job_id` bigint(20) unsigned NOT NULL,
+  `status` tinyint(3) unsigned NOT NULL DEFAULT 0 COMMENT '0 - waiting, 1 - accepted, 2 - rejeccted',
+  PRIMARY KEY (`employee_id`,`job_id`),
+  KEY `FK__employee_job__job_id` (`job_id`),
+  CONSTRAINT `FK__employee_job__employee_id` FOREIGN KEY (`employee_id`) REFERENCES `employee` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK__employee_job__job_id` FOREIGN KEY (`job_id`) REFERENCES `job` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
+
+-- Dumping data for table jobsite-db.employee_applied_job: ~2 rows (approximately)
+/*!40000 ALTER TABLE `employee_applied_job` DISABLE KEYS */;
+INSERT INTO `employee_applied_job` (`employee_id`, `job_id`, `status`) VALUES
+	(1, 2, 1),
+	(2, 2, 0);
+/*!40000 ALTER TABLE `employee_applied_job` ENABLE KEYS */;
+
+-- Dumping structure for table jobsite-db.employee_saved_job
+CREATE TABLE IF NOT EXISTS `employee_saved_job` (
+  `employee_id` bigint(20) unsigned NOT NULL,
+  `job_id` bigint(20) unsigned NOT NULL,
+  PRIMARY KEY (`employee_id`,`job_id`),
+  KEY `FK__employee_saved_job__job_id` (`job_id`),
+  CONSTRAINT `FK__employee_saved_job__employee_id` FOREIGN KEY (`employee_id`) REFERENCES `employee` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK__employee_saved_job__job_id` FOREIGN KEY (`job_id`) REFERENCES `job` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
+
+-- Dumping data for table jobsite-db.employee_saved_job: ~0 rows (approximately)
+/*!40000 ALTER TABLE `employee_saved_job` DISABLE KEYS */;
+/*!40000 ALTER TABLE `employee_saved_job` ENABLE KEYS */;
+
+-- Dumping structure for table jobsite-db.employer
+CREATE TABLE IF NOT EXISTS `employer` (
+  `ID` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) unsigned NOT NULL,
+  `company_id` bigint(20) unsigned NOT NULL,
+  `date_of_birth` date DEFAULT NULL,
+  `gender` varchar(16) COLLATE utf16_unicode_ci DEFAULT NULL,
+  `image_link` varchar(256) COLLATE utf16_unicode_ci DEFAULT NULL,
+  `status` tinyint(3) unsigned NOT NULL DEFAULT 0 COMMENT '0 - wait, 1 - accepted, 2 - rejected',
+  PRIMARY KEY (`ID`),
+  KEY `FK__employer__user` (`user_id`),
+  KEY `FK_employer_company` (`company_id`),
+  CONSTRAINT `FK__employer__user` FOREIGN KEY (`user_id`) REFERENCES `user` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK_employer_company` FOREIGN KEY (`company_id`) REFERENCES `company` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
+
+-- Dumping data for table jobsite-db.employer: ~1 rows (approximately)
+/*!40000 ALTER TABLE `employer` DISABLE KEYS */;
+INSERT INTO `employer` (`ID`, `user_id`, `company_id`, `date_of_birth`, `gender`, `image_link`, `status`) VALUES
+	(1, 4, 1, '2000-11-10', 'male', 'images/test1.png', 1);
+/*!40000 ALTER TABLE `employer` ENABLE KEYS */;
+
+-- Dumping structure for table jobsite-db.job
+CREATE TABLE IF NOT EXISTS `job` (
+  `ID` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `company_id` bigint(20) unsigned NOT NULL DEFAULT 0,
+  `title` varchar(128) COLLATE utf16_unicode_ci DEFAULT NULL,
+  `public_date` date DEFAULT NULL,
+  `expired_date` date DEFAULT NULL,
+  `field` varchar(64) COLLATE utf16_unicode_ci DEFAULT NULL,
+  `salary_min` bigint(20) unsigned DEFAULT NULL,
+  `salary_max` bigint(20) unsigned DEFAULT NULL,
+  `position` varchar(64) COLLATE utf16_unicode_ci DEFAULT NULL,
+  `type` varchar(32) COLLATE utf16_unicode_ci DEFAULT NULL,
+  `required_experience` varchar(64) COLLATE utf16_unicode_ci DEFAULT NULL,
+  `avaiable_slot` int(10) unsigned DEFAULT NULL,
+  `accepted_applicant` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `FK__job__company` (`company_id`),
+  CONSTRAINT `FK__job__company` FOREIGN KEY (`company_id`) REFERENCES `company` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
+
+-- Dumping data for table jobsite-db.job: ~1 rows (approximately)
+/*!40000 ALTER TABLE `job` DISABLE KEYS */;
+INSERT INTO `job` (`ID`, `company_id`, `title`, `public_date`, `expired_date`, `field`, `salary_min`, `salary_max`, `position`, `type`, `required_experience`, `avaiable_slot`, `accepted_applicant`) VALUES
+	(2, 1, 'ăn, ngủ', '2022-06-28', '2095-06-28', 'IT', 1000, 1000000, 'CEO', 'remote, ofline', '30 years', 1, 0),
+	(3, 2, 'vẽ', '2022-06-28', '2029-06-28', 'ART', 10000, 100000, 'Artist', 'remote', '15 years', 100, 30);
+/*!40000 ALTER TABLE `job` ENABLE KEYS */;
+
+-- Dumping structure for table jobsite-db.province
+CREATE TABLE IF NOT EXISTS `province` (
+  `ID` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) COLLATE utf16_unicode_ci NOT NULL,
+  `area_code` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
+
+-- Dumping data for table jobsite-db.province: ~0 rows (approximately)
+/*!40000 ALTER TABLE `province` DISABLE KEYS */;
+INSERT INTO `province` (`ID`, `name`, `area_code`) VALUES
+	(1, 'Hà Nội', 24),
+	(2, 'Hồ Chí Minh', 28);
+/*!40000 ALTER TABLE `province` ENABLE KEYS */;
 
 -- Dumping structure for table jobsite-db.socialaccount_socialaccount
 CREATE TABLE IF NOT EXISTS `socialaccount_socialaccount` (
@@ -357,12 +495,14 @@ CREATE TABLE IF NOT EXISTS `user` (
   `token` longtext COLLATE utf16_unicode_ci DEFAULT NULL,
   `token_expires` datetime DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
 
 -- Dumping data for table jobsite-db.user: ~1 rows (approximately)
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
 INSERT INTO `user` (`ID`, `username`, `password`, `first_name`, `last_name`, `social_account`, `social_account_id`, `social_auth_iss`, `joined_date`, `token`, `token_expires`) VALUES
-	(4, 'Nguyễn Hữu Kiệt', '28d3cf225a817ef98f79e87f85eca91080e6f77b41c21b342511bbee930f6600d65b598e61bdea4a40e5679f9f01713705b9998fb9fc813b29eabfcfea3a922406b708ddf3ed64ab74e84c0db156883b9870a5ab2d44470062182afad9f140ce3aa16762f4c3627ea6e2f174e8e0da82d9c9845642541f6f062d620ad85c78b2', 'Kiệt', 'Nguyễn Hữu', 'kietnguyen10112000@gmail.com', '114594750559756865595', 'https://accounts.google.com', '2022-06-13 21:13:15', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo0LCJleHAiOjE2NTUyMTU5OTYsImlhdCI6MTY1NTEyOTU5Nn0.c8IYCmVUOfEocdj9cw1fF77Tkn6A1e4Is4LAnGU_Vp8', '2022-06-14 14:13:16');
+	(4, 'Nguyễn Hữu Kiệt', '28d3cf225a817ef98f79e87f85eca91080e6f77b41c21b342511bbee930f6600d65b598e61bdea4a40e5679f9f01713705b9998fb9fc813b29eabfcfea3a922406b708ddf3ed64ab74e84c0db156883b9870a5ab2d44470062182afad9f140ce3aa16762f4c3627ea6e2f174e8e0da82d9c9845642541f6f062d620ad85c78b2', 'Kiệt', 'Nguyễn Hữu', 'kietnguyen10112000@gmail.com', '114594750559756865595', 'https://accounts.google.com', '2022-06-13 21:13:15', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo0LCJleHAiOjE2NTY0Mjk2MTksImlhdCI6MTY1NjM0MzIxOX0.7tM7BYWkAZvf9IHu7thjHclMggiaSevHTPCDENUlkL4', '2022-06-28 15:20:19'),
+	(6, 'tha_thu', 'c775e7b757ede630cd0aa1113bd102661ab38829ca52a6422ab782862f268646', NULL, NULL, 'haahha@gmail.com', NULL, NULL, '2022-06-20 22:36:26', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo2LCJleHAiOjE2NTU4MjU3ODcsImlhdCI6MTY1NTczOTM4N30.loAxbbVnwJBr1g0QheaKVGlfdJD59xlCtGcV8p6-dXE', '2022-06-21 15:36:27'),
+	(7, 'tha_thu2', 'c775e7b757ede630cd0aa1113bd102661ab38829ca52a6422ab782862f268646', NULL, NULL, 'haahha@gmail.com', NULL, NULL, '2022-06-27 21:23:33', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo3LCJleHAiOjE2NTY0MjYyMTMsImlhdCI6MTY1NjMzOTgxM30.geoSPnfSqrqJpxAJfFRi2ierOnjmbbUFlZ9SC0dCKZI', '2022-06-28 14:23:33');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 
 -- Dumping structure for table jobsite-db.user_role
@@ -371,7 +511,7 @@ CREATE TABLE IF NOT EXISTS `user_role` (
   `name` varchar(64) COLLATE utf16_unicode_ci DEFAULT NULL,
   `desc` varchar(256) COLLATE utf16_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`ID`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
 
 -- Dumping data for table jobsite-db.user_role: ~3 rows (approximately)
 /*!40000 ALTER TABLE `user_role` DISABLE KEYS */;
@@ -386,15 +526,16 @@ CREATE TABLE IF NOT EXISTS `user_role_relationship` (
   `user_id` bigint(20) unsigned NOT NULL,
   `user_role` bigint(20) unsigned NOT NULL,
   PRIMARY KEY (`user_id`,`user_role`),
-  KEY `FK__user_role` (`user_role`),
-  CONSTRAINT `FK__user` FOREIGN KEY (`user_id`) REFERENCES `user` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `FK__user_role` FOREIGN KEY (`user_role`) REFERENCES `user_role` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  KEY `FK__user_role_relationship__user_role` (`user_role`),
+  CONSTRAINT `FK__user_role_relationship__user` FOREIGN KEY (`user_id`) REFERENCES `user` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK__user_role_relationship__user_role` FOREIGN KEY (`user_role`) REFERENCES `user_role` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
 
--- Dumping data for table jobsite-db.user_role_relationship: ~1 rows (approximately)
+-- Dumping data for table jobsite-db.user_role_relationship: ~2 rows (approximately)
 /*!40000 ALTER TABLE `user_role_relationship` DISABLE KEYS */;
 INSERT INTO `user_role_relationship` (`user_id`, `user_role`) VALUES
-	(4, 1);
+	(4, 1),
+	(4, 2);
 /*!40000 ALTER TABLE `user_role_relationship` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
