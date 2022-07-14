9a1881bf134cd152e155e8aa7fe970708c2d1557
DangDuyAnh <duyanh.dang0109@gmail.com>
2022-07-14 10:53:41 +0700

    merge api

--- jobsite-client/src/routes/MyCV.vue
+++ jobsite-client/src/routes/MyCV.vue
@@ -1,5 +1,5 @@
 <script>
-import Navbar from '../components/Navbar.vue';
+import Navbar from '../components/Navbar2.vue';
 import { authenticationService } from '../utility/authenticationService';
 import axios from 'axios';
 export default {
@@ -10,7 +10,7 @@ export default {
     return {
       pdf : [],
       nowPDF: 0,
-      mainCV: 1,
+      mainCV: -1,
     }
   },
   mounted(){
@@ -28,6 +28,18 @@ export default {
           res.data.forEach(item => array.push(item.cv_id))
           this.pdf = [...array]
     })
+    axios.get('http://localhost:8000/api/employee/self-profile', config)
+      .then(res => {
+        if (res.data.main_cv_id === null || res.data.main_cv_id === null) {
+          this.mainCV = -1;
+          console.log('yes')
+          }
+        else { 
+          this.mainCV = res.data.main_cv_id;
+          console.log('no')
+          console.log(res.data.main_cv_id)
+        }
+      })
   },
   methods: {
     addCV() {
@@ -36,11 +48,34 @@ export default {
         'Authorization': 'Bearer ' + authenticationService.getUserToken()
         }
         }
-        axios.post('http://localhost:8000/api/employee/add-cv', {cv_id: String(this.nowPDF + 1)}, config)
+        axios.post('http://localhost:8000/api/employee/add-cv', {cv_id: String(this.nowPDF)}, config)
         .then(res => {
-          console.log(res.data)
-          console.log('DA dep trai')
+          this.pdf = [...this.pdf, this.nowPDF]
+          this.nowPDF += 1
         })
+    },
+    setMainCV(id) {
+        let config = {
+        headers: {
+        'Authorization': 'Bearer ' + authenticationService.getUserToken()
+        }
+        }
+        axios.post('http://localhost:8000/api/employee/set-main-cv', {cv_id: id}, config)
+        .then(res => {
+          this.mainCV = id
+        })
+    },
+    deleteM(id) {
+         let config = {
+        headers: {
+        'Authorization': 'Bearer ' + authenticationService.getUserToken()
+        }
+        }
+        axios.post('http://localhost:8000/api/employee/remove-cv', {cv_id: id}, config)
+        .then(res => {
+          let array = this.pdf.filter(item => item !== id)
+          this.pdf = [...array]
+        })     
     }
   }
 }
@@ -58,38 +93,20 @@ export default {
 
         <div class="image-CV-list">
             <div class="image-CV" v-for="(item, index) in pdf">
-              <img src='../assets/CV1.png' v-if="index === 0"/>
-              <img src='../assets/CV1.png' v-if="index === 1"/>
-              <img src='../assets/CV1.png' v-if="index === 2"/>
+              <img src='../assets/CV1.png' v-if="item === 0"/>
+              <img src='../assets/CV2.png' v-if="item === 1"/>
+              <img src='../assets/CV3.png' v-if="item === 2"/>
               <div class="CV-inner">
-              <div class="dat-lam-cv">
-              <button>Đặt làm CV chính</button>
+              <div class="dat-lam-cv" v-if="mainCV === item">
+              <button :style="{visibility: 'hidden'}">Đặt làm CV chính</button>
               <font-awesome-icon icon="star" :style="{color: 'yellow', fontSize: '18px'}"/>
               </div>
-              <p class="tieu-de">Tiêu đề 1</p>
-              <font-awesome-icon icon="trash" class='trash'/>
+              <div class="dat-lam-cv" v-else>
+              <button @click="setMainCV(item)">Đặt làm CV chính</button>
               </div>
-            </div>
-            <div class="image-CV">
-              <img src='../assets/CV2.png'/>
-              <div class="CV-inner">
-              <div class="dat-lam-cv">
-              <button>Đặt làm CV chính</button>
-              </div>
-              <div :style="{clear: 'both'}"></div>
-              <p class="tieu-de">Tiêu đề 1</p>
-              <font-awesome-icon icon="trash" class='trash'/>
-              </div>
-            </div>
-            <div class="image-CV">
-              <img src='../assets/CV3.png'/>
-              <div class="CV-inner">
-              <div class="dat-lam-cv">
-              <button>Đặt làm CV chính</button>
-              </div>
-              <div :style="{clear: 'both'}"></div>
-              <p class="tieu-de">Tiêu đề 1</p>
-              <font-awesome-icon icon="trash" class='trash'/>
+              <p class="tieu-de">CV {{item + 1}}</p>
+              <font-awesome-icon icon="trash" class='trash' v-if="mainCV === item"/>
+              <font-awesome-icon icon="trash" class='trash' @click="deleteM(item)" v-else/>
               </div>
             </div>
         </div>
--- jobsite-client/src/routes/MyLetter.vue
+++ jobsite-client/src/routes/MyLetter.vue
@@ -1,12 +1,81 @@
 <script>
-import Navbar from '../components/Navbar.vue';
+import Navbar from '../components/Navbar2.vue';
+import { authenticationService } from '../utility/authenticationService';
+import axios from 'axios';
 export default {
   components: {
     Navbar
   },
   data() {
     return {
-      pdf : ['']
+      pdf : [],
+      nowPDF: 0,
+      mainCV: -1,
+    }
+  },
+  mounted(){
+    console.log('user')
+    console.log(authenticationService.getUser())
+    let config = {
+    headers: {
+    'Authorization': 'Bearer ' + authenticationService.getUserToken()
+    }
+    }
+    axios.get('http://localhost:8000/api/employee/letter-list', config)
+        .then(res => {
+          this.nowPDF = res.data.length
+          let array = []
+          res.data.forEach(item => array.push(item.cv_id))
+          this.pdf = [...array]
+    })
+    axios.get('http://localhost:8000/api/employee/self-profile', config)
+      .then(res => {
+        if (res.data.main_cv_id === null || res.data.main_cv_id === null) {
+          this.mainCV = -1;
+          console.log('yes')
+          }
+        else { 
+          this.mainCV = res.data.main_cv_id;
+          console.log('no')
+          console.log(res.data.main_cv_id)
+        }
+      })
+  },
+  methods: {
+    addCV() {
+        let config = {
+        headers: {
+        'Authorization': 'Bearer ' + authenticationService.getUserToken()
+        }
+        }
+        axios.post('http://localhost:8000/api/employee/add-letter', {cv_id: String(this.nowPDF)}, config)
+        .then(res => {
+          this.pdf = [...this.pdf, this.nowPDF]
+          this.nowPDF += 1
+        })
+    },
+    setMainCV(id) {
+        let config = {
+        headers: {
+        'Authorization': 'Bearer ' + authenticationService.getUserToken()
+        }
+        }
+        axios.post('http://localhost:8000/api/employee/set-main-letter', {cv_id: id}, config)
+        .then(res => {
+          this.mainCV = id
+        })
+    },
+    deleteM(id) {
+         let config = {
+        headers: {
+        'Authorization': 'Bearer ' + authenticationService.getUserToken()
+        }
+        }
+        axios.post('http://localhost:8000/api/employee/remove-letter', {cv_id: id}, config)
+        .then(res => {
+          let array = this.pdf.filter(item => item !== id)
+          this.pdf = [...array]
+        })     
     }
   }
 }
@@ -23,39 +92,24 @@ export default {
         </div>
 
         <div class="image-CV-list">
-            <div class="image-CV">
-              <img src='../assets/Letter1.png'/>
+            <div class="image-CV" v-for="(item, index) in pdf">
+              <img src='../assets/Letter1.png' v-if="item === 0"/>
+              <img src='../assets/Letter2.png' v-if="item === 1"/>
+              <img src='../assets/Letter3.png' v-if="item === 2"/>
               <div class="CV-inner">
-              <div class="dat-lam-cv">
-              <button>Đặt làm CV chính</button>
+              <div class="dat-lam-cv" v-if="mainCV === item">
+              <button :style="{visibility: 'hidden'}">Đặt làm Thư xin việc chính</button>
               <font-awesome-icon icon="star" :style="{color: 'yellow', fontSize: '18px'}"/>
               </div>
-              <p class="tieu-de">Tiêu đề 1</p>
-              <font-awesome-icon icon="trash" class='trash'/>
+              <div class="dat-lam-cv" v-else>
+              <button @click="setMainCV(item)">Đặt làm Thư xin việc chính</button>
+              </div>
+              <p class="tieu-de">CV {{item + 1}}</p>
+              <font-awesome-icon icon="trash" class='trash' v-if="mainCV === item"/>
+              <font-awesome-icon icon="trash" class='trash' @click="deleteM(item)" v-else/>
               </div>
             </div>
-            <div class="image-CV">
-              <img src='../assets/Letter2.png'/>
-              <div class="CV-inner">
-              <div class="dat-lam-cv">
-              <button>Đặt làm CV chính</button>
-              </div>
-              <div :style="{clear: 'both'}"></div>
-              <p class="tieu-de">Tiêu đề 1</p>
-              <font-awesome-icon icon="trash" class='trash'/>
-              </div>
-            </div>
-            <div class="image-CV">
-              <img src='../assets/Letter3.png'/>
-              <div class="CV-inner">
-              <div class="dat-lam-cv">
-              <button>Đặt làm CV chính</button>
-              </div>
-              <div :style="{clear: 'both'}"></div>
-              <p class="tieu-de">Tiêu đề 1</p>
-              <font-awesome-icon icon="trash" class='trash'/>
-              </div>
-            </div>
+            
         </div>
       </div>
     </div>
