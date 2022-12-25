5a9ea295a1869d449a9cc3bfa457a3e09301b716
Nguyen Huu Kiet <kietnguyen10112000@gmail.com>
2022-12-26 01:24:34 +0700

    - bug fix

--- jobsite-server/jobsite/main/employee/api.py
+++ jobsite-server/jobsite/main/employee/api.py
@@ -340,12 +340,14 @@ class JobRecommend(APIView):
         ret = []
         for key, value in personalized_results.items():
             job_node = jrec.G.nodes[key]
+            company_id = job_node['company_id']
             ret.append({
-                'company_id'    : job_node['company_id'],
+                'company_id'    : company_id,
                 'job_name'      : job_node['job_name'],
                 'taglist'       : job_node['taglist'],
                 'location'      : job_node['location'],
                 'description'   : job_node['description'],
+                'logo_link'     : jrec.G.nodes[company_id]['logo_link'],
             })
             
         return Response(ret)
